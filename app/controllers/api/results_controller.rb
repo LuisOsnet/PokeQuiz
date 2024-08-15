fc44a45630fb5ffc08fafd2fd902891bb8394ab0
Luis Osnet <luisosnet@gmail.com>
2024-08-15 11:03:27 -0600

    fix authentication

--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -1,8 +1,10 @@
-class Api::ResultsController < ApplicationController
-  def index
-    @users = User.includes(:games)
-    render :index, status: :ok
-  rescue => e
-    error(:bad_request, e&.message)
+module Api
+  class ResultsController < ApplicationController
+    def index
+      @users = User.includes(:games)
+      render :index, status: :ok
+    rescue => e
+      error(:bad_request, e&.message)
+    end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery prepend: true
   before_action :authenticate_user!
   before_action :set_locale
-  include Api::ErrorsHandler
 
   def after_sign_in_path_for(resource)
     games_path
@@ -12,9 +11,5 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
-  end
-
-  def unauthenticated
-    error(:unauthorized)
   end
 end
