e41ed6e24fda213b361a00e6598ff19da25a23bd
Luis Osnet <luisosnet@gmail.com>
2024-08-15 11:19:21 -0600

    fix typos and result controller

--- app/controllers/api/results_controller.rb
+++ app/controllers/api/results_controller.rb
@@ -1,10 +1,20 @@
 module Api
-  class ResultsController < ApplicationController
+  class ResultsController < Api::ApplicationController
     def index
-      @users = User.includes(:games)
+      @users = fetch_users_with_games
       render :index, status: :ok
-    rescue => e
-      error(:bad_request, e&.message)
+    rescue StandardError => e
+      handle_error(e)
+    end
+
+    private
+
+    def fetch_users_with_games
+      User.includes(:games).where.not(games: { id: nil })
+    end
+
+    def handle_error(exception)
+      error(:bad_request, exception.message)
     end
   end
 end
